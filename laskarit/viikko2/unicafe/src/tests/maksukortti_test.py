6f6b7f09e89aa40f5bf4fe22a93b48555f1d5167
ttanninen <teemu@TEPU.localdomain>
2026-03-19 00:14:45 +0200

    Tehtävä 6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -7,3 +7,18 @@ class TestMaksukortti(unittest.TestCase):
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alustetaan_oikein(self):
+        self.assertEqual(self.maksukortti.saldo_euroina(), 10.00)
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(1000)
+        self.assertEqual(self.maksukortti.saldo_euroina(), 20.00)
+
+    def test_saldo_vahenee_jos_tarpeeksi_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(500), True)
+    
+    def test_saldo_ei_muutu_jos_ei_tarpeeksi_rahaa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1500), False)
+
+    
