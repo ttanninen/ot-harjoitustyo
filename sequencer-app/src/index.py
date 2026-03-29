5ce5cdc0596600eff9b8da27c4d95960cec5af64
ttanninen <teemu@TEPU.localdomain>
2026-03-30 01:08:06 +0300

    Functionality testing with single track

--- sequencer-app/src/index.py
+++ sequencer-app/src/index.py
@@ -1,17 +1,20 @@
-import clock, audioengine, sequencer
+import sequencer
+import time
 
 # Create new sequence
-sequence = sequencer.Sequence(128, [])
+sequence = sequencer.Sequence(128, 4)
 
 # Create new track
-track1 = sequencer.Track("/samples/bd01.wav", "BD")
+track1 = sequencer.Track("sequencer-app/src/samples/bd01.wav", "BD")
 
 # Create 8-step pattern
-track1.add_pattern([1,0,0,0, 1,0,1,0])
+track1.replace_pattern([1,0,0,0, 1,0,1,0])
 
 # Load track 1 to sequence
 sequence.add_track(track1)
 
-running = True
+sequence.play()
 
-while running:
+time.sleep(4)
+
+sequence.stop()
