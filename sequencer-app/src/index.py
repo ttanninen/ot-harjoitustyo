3beb6ff799f8b23aad6b134f81a34ed5b0530188
ttanninen <teemu@TEPU.localdomain>
2026-03-30 23:06:36 +0300

    Added logical steps for testing the pattern write functionality

--- sequencer-app/src/index.py
+++ sequencer-app/src/index.py
@@ -1,5 +1,4 @@
 import sequencer
-import time
 
 # Create new sequence
 sequence = sequencer.Sequence(128, 4)
@@ -7,8 +6,14 @@ sequence = sequencer.Sequence(128, 4)
 # Create new track
 track1 = sequencer.Track("sequencer-app/src/samples/bd01.wav", "BD")
 
-# Create 8-step pattern
-track1.replace_pattern([1,0,0,0, 1,0,1,0])
+# Create 16-step pattern
+track1.set_length(16)
+
+# Write some notes
+track1.write_step(0)
+track1.write_step(4)
+track1.write_step(8)
+track1.write_step(12)
 
 # Load track 1 to sequence
 sequence.add_track(track1)
