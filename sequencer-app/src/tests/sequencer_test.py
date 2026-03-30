c48bf584ffad41cad3711e01046520d85842e19d
ttanninen <teemu@TEPU.localdomain>
2026-03-31 01:06:03 +0300

    Added task invoke, unittests and coverage

---
+++ sequencer-app/src/tests/sequencer_test.py
@@ -0,0 +1,25 @@
+import unittest
+from sequencer import Track, Sequence
+
+class testTrack(unittest.TestCase):
+    def setUp(self):
+        filename = "testfile.wav"
+        self.track = Track(filename, "test_name")
+    
+    def test_track_length_setting(self):
+        self.track.set_length(16)
+        
+        self.assertEqual(len(self.track.pattern), 16)
+
+    def test_track_step_write(self):
+        self.track.set_length(8)
+        self.track.write_step(4)
+
+        self.assertEqual(self.track.pattern[4], 1)
+
+    def test_track_step_erase(self):
+        self.track.set_length(8)
+        self.track.write_step(4)
+        self.track.erase_step(4)
+
+        self.assertEqual(self.track.pattern[4], 0)
