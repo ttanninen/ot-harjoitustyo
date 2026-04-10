949880aa7aef4e558f006c206af3a214a65786c1
ttanninen <teemu@TEPU.localdomain>
2026-04-11 00:05:02 +0300

    Added underscore to internal variables

--- sequencer-app/src/audioengine.py
+++ sequencer-app/src/audioengine.py
@@ -24,28 +24,28 @@ class AudioEngine:
     def __init__(self, samplerate=44100, blocksize=512):
         self.samplerate = samplerate
         self.blocksize = blocksize
-        self.sounds = [] # Sounds in playback queue
+        self._sounds = [] # Sounds in playback queue
         
         # Initialize stereo audio stream where audio files are inserted
-        self.stream = sd.OutputStream(
+        self._stream = sd.OutputStream(
             samplerate= self.samplerate,
             blocksize= self.blocksize,
             channels=2,
-            callback=self.callback
+            callback=self._callback
         )
 
     def start(self):
-        self.stream.start()
+        self._stream.start()
 
     def stop(self):
-        self.stream.stop()
+        self._stream.stop()
 
     def play(self, sample, volume=1.0, pan=0.0):
-        self.sounds.append(Audio_file(sample, volume, pan))
+        self._sounds.append(Audio_file(sample, volume, pan))
 
     ### AI-generated code begins ###
 
-    def callback(self, outdata, frames, time, status):
+    def _callback(self, outdata, frames, time, status):
         # Create empty output buffer
         buffer = np.zeros((frames, 2), dtype="float32")
 
@@ -53,7 +53,7 @@ class AudioEngine:
         active_sounds = []
 
         # Go through sounds which are queued to play
-        for sound in self.sounds:
+        for sound in self._sounds:
             # Calculate how much of playback is remaining
             remaining = len(sound.data) - sound.position
             # Set sound slice length to be copied to the output buffer
@@ -80,7 +80,7 @@ class AudioEngine:
                 active_sounds.append(sound)
 
         # Remove finished sounds from playback
-        self.sounds = active_sounds
+        self._sounds = active_sounds
 
         # Prevent audio buffer from clipping:
         buffer = np.clip(buffer, -1.0, 1.0)
