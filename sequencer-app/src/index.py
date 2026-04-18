8f86dacaae34bb8359264b69bd49f10f7c0fddaa
ttanninen <teemu@TEPU.localdomain>
2026-04-18 21:00:02 +0300

    Moved application context management to app.py. Index retains only program initialization

--- sequencer-app/src/index.py
+++ sequencer-app/src/index.py
@@ -1,45 +1,33 @@
 import os
 import tkinter as tk
 
+from app import App
 from ui.ui import UI
-from services.sequencer import Sequence, Track
-from services.audioengine import AudioEngine
 
 def main():
+    # Start app context
+    app = App()
+    app.start()
 
-    # Add initial sample files
-
+    # Add initial samples and tracks
     dirname = os.path.dirname(__file__)
 
     kick = os.path.join(dirname, "samples", "bd01.wav")
     snare = os.path.join(dirname, "samples", "sd01.wav")
     hihat = os.path.join(dirname, "samples", "ch01.wav")
 
-    # Start audio engine
-
-    engine = AudioEngine()
-    engine.start()
-
-    # Initialize demo sequence
-
-    seq = Sequence(bpm=120, steps_per_beat=4, engine=engine)
-
-    kick_sample = Track(kick,  "Kick",  pattern=[
+    app.add_track(kick,  "Kick",  pattern=[
                         1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0])
-    snare_sample = Track(snare, "Snare", pattern=[
+    app.add_track(snare, "Snare", pattern=[
                         0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0])
-    hihat_sample = Track(hihat, "Hi-hat", pattern=[
+    app.add_track(hihat, "Hi-hat", pattern=[
                         0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0])
 
-    seq.add_track(kick_sample)
-    seq.add_track(snare_sample)
-    seq.add_track(hihat_sample)
-
-    # Initialize GUI and run main loop
+    # Start GUI
     root = tk.Tk()
-    app = UI(root, seq)
+    ui = UI(root, app)
     root.mainloop()
-    engine.stop()
+    app.stop()
 
 if __name__ == "__main__":
     main()
