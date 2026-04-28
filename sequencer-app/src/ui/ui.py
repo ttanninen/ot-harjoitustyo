bce8da063c42a90c0098905f5c98d4177c38cd30
ttanninen <teemu@TEPU.localdomain>
2026-04-28 21:33:37 +0300

    Fix whitespace

--- sequencer-app/src/ui/ui.py
+++ sequencer-app/src/ui/ui.py
@@ -72,7 +72,7 @@ class UI:
                                   command=self._add_track)
         add_track_btn.pack(side=tk.RIGHT)
         add_track_btn.bind("<space>", lambda e:self._toggle_play() or "break")
-        
+
         clear_pattern_btn = tk.Button(toolbar, text="Clear pattern", width=20,
                                       command=self._clear_pattern)
         clear_pattern_btn.pack(side=tk.RIGHT)
