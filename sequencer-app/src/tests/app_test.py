d2a8f08a748756fec1c887172888fd8e72a894e1
ttanninen <teemu@TEPU.localdomain>
2026-05-10 02:37:12 +0300

    unittests

--- sequencer-app/src/tests/app_test.py
+++ sequencer-app/src/tests/app_test.py
@@ -6,21 +6,21 @@ class testApp(unittest.TestCase):
         self.app = app.App()
     
     def test_start_stream(self):
-        self.app.engine.start()
+        self.app.start()
 
         self.assertTrue(self.app.engine._device.running)
 
     def test_stop_stream(self):
-        self.app.engine.start()
-        self.app.engine.stop()
+        self.app.start()
+        self.app.stop()
 
         self.assertFalse(self.app.engine._device.running)
 
     def test_stop_playback(self):
-        self.app.engine.start()
-        self.app.engine.stop()
+        self.app.start()
+        self.app.stop()
 
         self.assertFalse(self.app.sequence._playing)
 
     def tearDown(self):
-        self.app.engine.stop()
+        self.app.stop()
